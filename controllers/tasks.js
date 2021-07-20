e1de06317bbee3732076986ec44539b89279dc3d
Kurtuluş ERTÜRK <47227072+kurtuluserturk@users.noreply.github.com>
2021-07-20 09:59:22 +0300

    all controllers added

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,11 +1,47 @@
-const getAllTasks = async (req, res) => {
+const postTask = async (req, res) => {
   try {
-    res.send('all tasks')
+    res.send('post task')
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+const getTasks = async (req, res) => {
+  try {
+    res.send('get all tasks')
   } catch (err) {
     console.log(err)
   }
 }
 
+const getTask = async (req, res) => {
+  try {
+    res.send('get single task')
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+const patchTask = async (req, res) => {
+  try {
+    res.send('update task with patch')
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+const deleteTask = async (req, res) => {
+  try {
+    res.send('delete task')
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 module.exports = {
-  getAllTasks
+  postTask,
+  getTasks,
+  getTask,
+  patchTask,
+  deleteTask
 }
